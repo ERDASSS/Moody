e0f04be0e01df1a543d536221d765b734b21e32d
ERDASSS <com123465@mail.ru>
2024-11-05 19:58:54 +0500

    Тест

---
+++ test/Program.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Text;
+using VkNet;
+using VkNet.AudioBypassService.Extensions;
+using VkNet.Enums.Filters;
+using VkNet.Model;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var services = new ServiceCollection();
+        services.AddAudioBypass();
+        var api = new VkApi(services);
+
+        //api.Authorize(new ApiAuthParams
+        //{
+        //    AccessToken = "vk1.a.kp-N_lQmGAVMYcHQb_Rv8UcgwzRIfc4R9N559CJOt5MbH61QwmgoVkBYBaKVqp2oG0dRLPQU1-nUWWfKawvdn39kyrePlcXJKmZQkvRfEooHgb_yumndd-9nDu7Y1gOHjRwd3-kJ5tdYp4sZdLZfczLtciOzL_ETyxHvKdqesMGkoygILmm4xsUrHMMngGMVDUiUz86Y8S3Xv4v66-b3_w"
+        //});
+        Console.WriteLine("Введите логин");
+        var login = Console.ReadLine();
+        Console.WriteLine("Введите пароль");
+        var password = Console.ReadLine();
+        api.Authorize(new ApiAuthParams
+        {
+            ApplicationId = 52614150,
+            Login = login,
+            Password = password,
+            TwoFactorAuthorization = new Func<string>(() =>
+            {
+                Console.WriteLine("Введите код 2FA:");
+                var code = Console.ReadLine();
+                return code;
+            }),
+            Settings = Settings.All
+        });
+        var id = api.UserId;
+        Console.WriteLine(api.Token);
+        //var song = "318348717_456239095".Split();
+        //api.Audio.CreatePlaylist(318348717, "СЮДАА", "", song);
+        var musicList = api.Audio.Get(new VkNet.Model.RequestParams.AudioGetParams { OwnerId = id });
+        var songToPlayList = new StringBuilder();
+        for (int i = 0; i < 5; i ++)
+        {
+            songToPlayList.Append($"{id}_{musicList[i].Id},");
+            var x = api.Audio.GetById($"{id}_{musicList[i].Id}".Split());
+        }
+        songToPlayList.Remove(songToPlayList.Length - 1, 1);
+
+        var songsInPlayList = songToPlayList.ToString().Split(',');
+        var playlist = api.Audio.CreatePlaylist((long)id, "Тесты2");
+        foreach (var song in songsInPlayList)
+        {
+            api.Audio.AddToPlaylist((long)id, (long)playlist.Id, song.Split());
+        }
+
+        //var asd = api.Gifts.Get();
+        //var x = api.Audio.Get(a);
+        //api.Audio.AddToPlaylist((long)u.OwnerId, (long)u.Id, );
+        Console.WriteLine("Complete");
+    }
+}
